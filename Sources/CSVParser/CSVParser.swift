76c2bbff9c4a3124a8c3fcc38a1ff0042a691015
Michael Salmon <msalmonse@gmail.com>
2021-04-16 08:35:26 +0200

    Initial commit

--- Sources/CSVParser/CSVParser.swift
+++ Sources/CSVParser/CSVParser.swift
@@ -1,3 +1,123 @@
-struct CSVParser {
-    var text = "Hello, World!"
+#if os(macOS)
+import Foundation
+#endif
+
+/// State of parser
+
+private enum ParserState: Comparable {
+    case
+        lineStart,          // no field data found, row is empty or whitespace
+        fieldStart,         // field started but no data found, like lineStart but at least one field already
+        normal,             // non-whitespace found
+        qdfound,            // double quote found while quoted
+        quoted              // quoted text started
+}
+
+/// Parse csv string
+/// - Parameters:
+///   - inData: the string to parse
+///   - separatedBy: field separator
+///   - to: Array of arrays to hold results
+//    - leavingWhiteSpace: Should white space be trimmed or left?
+
+func csvParse(
+    _ inData: String,
+    separatedBy: String = ",",
+    to outData: inout [[String]],
+    leavingWhiteSpace: Bool = false
+) {
+    let trimWS = !leavingWhiteSpace
+    let colsep = separatedBy.unicodeScalars.first
+    let cr: UnicodeScalar = "\r"
+    let qd: UnicodeScalar = "\""
+    let nl: UnicodeScalar = "\n"
+    let space: UnicodeScalar = " "
+    var state = ParserState.lineStart
+    var field = ""
+    var spaceCount = 0              // count of potentially trailing spaces
+    var lastRow = -1                // the index of the last row in outData
+    var ws = CharacterSet()
+    ws.insert(charactersIn: "\r\n ")
+
+    // clean up output
+    outData = []
+
+    for ch in inData.unicodeScalars {
+        // Valid data found so add a row?
+        if state == .lineStart && !(trimWS && ws.contains(ch)) {
+            outData.append([])
+            lastRow += 1
+            assert(lastRow == outData.endIndex - 1, "outData indices don't match")
+        }
+
+        // handle copying to field and next state
+        switch (state, ch, trimWS) {
+            // when quoted colsep, cr and nl are normal characters
+        case (.quoted, colsep, _),
+             (.quoted, cr, _),
+             (.quoted, nl, _):
+            field.unicodeScalars.append(ch)
+
+            // skip carriage return
+            // if trimWS skip spaces at the start of a line or field, nl if we haven't started a field
+        case (_, cr, _),
+             (.fieldStart, space, true),
+             (.lineStart, space, true),
+             (.lineStart, nl, true):
+            break
+
+            // in every other state colsep terminates a cell, new line always terminates a cell
+        case (_, colsep, _),
+             (_, nl, _):
+            if spaceCount > 0 { field.unicodeScalars.removeLast(spaceCount) }
+            outData[lastRow].append(field)
+            field = ""
+            state = ch == nl ? .lineStart : .fieldStart
+            spaceCount = 0
+
+            // double quote normally puts us into quoted
+        case (.lineStart, qd, _),
+             (.fieldStart, qd, _),
+             (.normal, qd, _):
+            state = .quoted
+
+            // while in quoted state a double quote may terminate the state or be an actual double quote
+        case (.quoted, qd, _): state = .qdfound
+
+            // two double quotes while quoted means a literal double quote
+        case (.qdfound, qd, _):
+            field.unicodeScalars.append(ch)
+            state = .quoted
+
+            // one means the end of the quote, append that character
+        case (.qdfound, _, _):
+            field.unicodeScalars.append(ch)
+            state = .normal
+
+            // if in lineStart or fieldStart go to normal
+        case (.lineStart, _, _),
+             (.fieldStart, _, _):
+            state = .normal
+            field.unicodeScalars.append(ch)
+
+            // otherwise just add the character to the cell
+        default:
+            field.unicodeScalars.append(ch)
+        }
+
+        // handle spaceCount
+        if trimWS {
+            switch (state, ch) {
+            case (.quoted, _): spaceCount = 0
+            case (.normal, space): spaceCount += 1
+            default:
+                spaceCount = 0
+            }
+        }
+    }
+
+    if state != .lineStart {
+        if spaceCount > 0 { field.unicodeScalars.removeLast(spaceCount) }
+        outData[lastRow].append(field)
+    }
 }
--- Tests/CSVParserTests/CSVParserTests.swift
+++ Tests/CSVParserTests/CSVParserTests.swift
@@ -2,14 +2,116 @@ import XCTest
 @testable import CSVParser
 
 final class CSVParserTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CSVParser().text, "Hello, World!")
+
+    func csvGen(_ rows: Int, by cols: Int, precision: Int = 4) -> String {
+        var data: [String] = []
+
+        for _ in 0..<rows {
+            var row: [String] = []
+            for _ in 0..<cols {
+                row.append(String(format: "%*d", precision, Int.random(in: -500...500)))
+            }
+            data.append(row.joined(separator: ","))
+        }
+        return data.joined(separator: "\r\n")
     }
 
+    func testTrimmingCSVParse() {
+        var parsed: [[String]] = []
+        csvParse(Self.testRow, to: &parsed)
+        XCTAssertEqual(parsed.count, 1)
+        XCTAssertEqual(parsed[0].count, 6)
+        XCTAssertEqual(parsed[0][2], "Test with \"  and emoji 🌊")
+
+        csvParse(Self.csvData, to: &parsed)
+        XCTAssertEqual(parsed.count, 5)
+        for row in parsed {
+            XCTAssertEqual(row.count, 5)
+        }
+
+        csvParse("        ", to: &parsed)
+        XCTAssertEqual(parsed.count, 0)
+
+        csvParse("        ,", to: &parsed)
+        XCTAssertEqual(parsed.count, 1)
+        XCTAssertEqual(parsed[0].count, 2)
+
+        csvParse("        \",\" ", to: &parsed)
+        XCTAssertEqual(parsed.count, 1)
+        XCTAssertEqual(parsed[0].count, 1)
+
+        csvParse("        \"\r\n\" ", to: &parsed)
+        XCTAssertEqual(parsed.count, 1)
+        XCTAssertEqual(parsed[0].count, 1)
+
+        csvParse("1,2,3\r\n \n4,5,6", to: &parsed)
+        XCTAssertEqual(parsed.count, 2)
+        XCTAssertEqual(parsed[0].count, 3)
+        XCTAssertEqual(parsed[1].count, 3)
+    }
+
+    func testNoTrimmingCSVParse() {
+        var parsed: [[String]] = []
+        csvParse(Self.testRow, to: &parsed, leavingWhiteSpace: true)
+        XCTAssertEqual(parsed.count, 1)
+        XCTAssertEqual(parsed[0].count, 6)
+        XCTAssertEqual(parsed[0][2], " Test with \"  and emoji 🌊 ")
+
+        csvParse(Self.csvData, to: &parsed, leavingWhiteSpace: true)
+        XCTAssertEqual(parsed.count, 5)
+        for row in parsed {
+            XCTAssertEqual(row.count, 5)
+        }
+
+        csvParse("        ", to: &parsed, leavingWhiteSpace: true)
+        XCTAssertEqual(parsed.count, 1)
+
+        csvParse("        ,", to: &parsed, leavingWhiteSpace: true)
+        XCTAssertEqual(parsed.count, 1)
+        XCTAssertEqual(parsed[0].count, 2)
+
+        csvParse("        \",\" ", to: &parsed, leavingWhiteSpace: true)
+        XCTAssertEqual(parsed.count, 1)
+        XCTAssertEqual(parsed[0].count, 1)
+
+        csvParse("        \"\r\n\" ", to: &parsed, leavingWhiteSpace: true)
+        XCTAssertEqual(parsed.count, 1)
+        XCTAssertEqual(parsed[0].count, 1)
+
+        csvParse("1,2,3\r\n \n4,5,6", to: &parsed, leavingWhiteSpace: true)
+        XCTAssertEqual(parsed.count, 3)
+        XCTAssertEqual(parsed[0].count, 3)
+        XCTAssertEqual(parsed[1].count, 1)
+        XCTAssertEqual(parsed[2].count, 3)
+    }
+
+    func testCsvParsePerformance() {
+        var parsed: [[String]] = []
+        let testData = csvGen(100000, by: 7, precision: 8)
+        measure {
+            csvParse(testData, to: &parsed)
+        }
+    }
+
+    static let testName = "Ozzymandis"
+
+    // CSV string for parser test
+    static let testRow = """
+      1  , 234, "Test with "" " and emoji 🌊 ,,"\r\n",1\r
+    """
+
+    // CSV string for tests
+    static let csvData = """
+    1,-1,9,3 " "" " 5,"\(testName)"\r
+    1,100.1,120.4, -110.1,0.0\r
+    9,100.1 ,129.9,5220.6 ,0.0\r
+    32,100.1,152.7,,\r
+    "\(testName)",,,,0.0\r
+    """
+
     static var allTests = [
-        ("testExample", testExample),
+        ("test csvParse with ws trimming", testTrimmingCSVParse),
+        ("test csvParse without ws trimming", testNoTrimmingCSVParse),
+        ("test csvParse performance", testCsvParsePerformance)
     ]
 }
